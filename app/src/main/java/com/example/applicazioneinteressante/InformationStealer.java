9f664a2421dd4f62598facf38917d769b5a3a165
Matteo Marino <95318540+Mathieus99@users.noreply.github.com>
2023-05-10 11:54:44 +0200

    Initial commit
    Replacing old repository

---
+++ app/src/main/java/com/example/applicazioneinteressante/InformationStealer.java
@@ -0,0 +1,6 @@
+package com.example.applicazioneinteressante;
+
+public class InformationStealer {
+
+
+}
---
+++ app/src/main/java/com/example/applicazioneinteressante/NetworkManager.java
@@ -0,0 +1,17 @@
+package com.example.applicazioneinteressante;
+
+public class NetworkManager {
+
+    //Variabile connection
+
+    public static void openConnection(){
+
+    }
+
+    public static void closeConnection(){
+
+    }
+
+
+
+}

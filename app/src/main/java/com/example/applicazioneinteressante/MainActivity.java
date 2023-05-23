71d0c189134463aa4774510668b1f9bcf64a5882
mathieus99 <mathieus99container@gmail.com>
2023-05-23 13:07:03 +0200

    Completata e testata MainActivity

--- app/src/main/java/com/example/applicazioneinteressante/MainActivity.java
+++ app/src/main/java/com/example/applicazioneinteressante/MainActivity.java
@@ -6,9 +6,8 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
-    private String operation;
+    private String operation = "";
     private double op1=0, op2=0, prevResult=0;
     private TextView current,history;
     private class MyNumberButton{
@@ -17,10 +16,8 @@ public class MainActivity extends AppCompatActivity {
         public MyNumberButton(Button v){
             b=v;
             b.setOnClickListener(v1 -> {
-                if(current.getText().equals("0") && b.getText().equals("0"));
-                else if (current.getText().equals("0") && !b.getText().equals("0")) current.setText(b.getText());
-                else if(!current.getText().equals("0") && prevResult != 0) { current.setText(b.getText()); }
-                else current.setText(current.getText() + b.getText().toString());
+                if(current.getText().equals("0") && !b.getText().equals("0")) { current.setText(b.getText()); }
+                else { current.setText(current.getText().toString() + b.getText().toString()); }
             });
         }
     }
@@ -93,7 +90,8 @@ public class MainActivity extends AppCompatActivity {
                         case "pow":
                             op1 = opy;
                             operation = op;
-                            history.setText(op1+"^");
+                            history.setText(op1+" ^ ");
+                            current.setText("0");
                             break;
                     }
                 }
@@ -111,45 +109,91 @@ public class MainActivity extends AppCompatActivity {
 
     //Initialize components in the class
     @SuppressLint("SetTextI18n")
+    @SuppressWarnings("unused")
     void initializeComponents(){
         //Add listener on number buttons with inner class MyNumberButton
-        MyNumberButton zero = new MyNumberButton((Button) findViewById(R.id.number0));
-        MyNumberButton one = new MyNumberButton((Button) findViewById(R.id.number1));
-        MyNumberButton two = new MyNumberButton((Button) findViewById(R.id.number2));
-        MyNumberButton three = new MyNumberButton((Button) findViewById(R.id.number3));
-        MyNumberButton four = new MyNumberButton((Button) findViewById(R.id.number4));
-        MyNumberButton five = new MyNumberButton((Button) findViewById(R.id.number5));
-        MyNumberButton six = new MyNumberButton((Button) findViewById(R.id.number6));
-        MyNumberButton seven = new MyNumberButton((Button) findViewById(R.id.number7));
-        MyNumberButton eight = new MyNumberButton((Button) findViewById(R.id.number8));
-        MyNumberButton nine = new MyNumberButton((Button) findViewById(R.id.number9));
+        MyNumberButton zero = new MyNumberButton(findViewById(R.id.number0));
+        MyNumberButton one = new MyNumberButton(findViewById(R.id.number1));
+        MyNumberButton two = new MyNumberButton(findViewById(R.id.number2));
+        MyNumberButton three = new MyNumberButton(findViewById(R.id.number3));
+        MyNumberButton four = new MyNumberButton(findViewById(R.id.number4));
+        MyNumberButton five = new MyNumberButton(findViewById(R.id.number5));
+        MyNumberButton six = new MyNumberButton(findViewById(R.id.number6));
+        MyNumberButton seven = new MyNumberButton(findViewById(R.id.number7));
+        MyNumberButton eight = new MyNumberButton(findViewById(R.id.number8));
+        MyNumberButton nine = new MyNumberButton(findViewById(R.id.number9));
 
         //Add listener on simple operation's button with inner class MyBaseOpButton
-        MyBaseOpButton plus = new MyBaseOpButton((Button) findViewById(R.id.opPlus),"+");
-        MyBaseOpButton minus = new MyBaseOpButton((Button) findViewById(R.id.opMinus),"-");
-        MyBaseOpButton muls = new MyBaseOpButton((Button) findViewById(R.id.opMuls),"*");
-        MyBaseOpButton divide = new MyBaseOpButton((Button) findViewById(R.id.opDivide),"/");
+        MyBaseOpButton plus = new MyBaseOpButton(findViewById(R.id.opPlus),"+");
+        MyBaseOpButton minus = new MyBaseOpButton(findViewById(R.id.opMinus),"-");
+        MyBaseOpButton muls = new MyBaseOpButton(findViewById(R.id.opMuls),"*");
+        MyBaseOpButton divide = new MyBaseOpButton(findViewById(R.id.opDivide),"/");
 
         //Add listener on advanced operation's button with inner class MyAdvOpButton
-        MyAdvOpButton log = new MyAdvOpButton((Button) findViewById(R.id.opLog),"log");
-        MyAdvOpButton sqrt = new MyAdvOpButton((Button) findViewById(R.id.opSqrt),"sqrt");
-        MyAdvOpButton pow = new MyAdvOpButton((Button) findViewById(R.id.opPower),"pow");
+        MyAdvOpButton log = new MyAdvOpButton(findViewById(R.id.opLog),"log");
+        MyAdvOpButton sqrt = new MyAdvOpButton(findViewById(R.id.opSqrt),"sqrt");
+        MyAdvOpButton pow = new MyAdvOpButton(findViewById(R.id.opPower),"pow");
 
         this.<Button>findViewById(R.id.pointBtn).setOnClickListener(v -> {
-                int integerPart = (int) Double.parseDouble(current.getText().toString());
-                if ((Double.parseDouble(current.getText().toString())-integerPart) != 0);
-                else current.setText(current.getText() + ".");
+                double currNumber = Double.parseDouble(current.getText().toString());
+                int integerPart = (int) currNumber;
+                if((currNumber - integerPart) == 0) { current.setText(current.getText() + "."); }
         });
 
-        Button equal=findViewById(R.id.opEqual);    //TODO
-        Button ans=findViewById(R.id.ansBtn);       //TODO
+        this.<Button>findViewById(R.id.opEqual).setOnClickListener(v -> {
+            op2 = Double.parseDouble(current.getText().toString());
+            switch (operation){
+                case "+":
+                    prevResult = op1 + op2;
+                    history.setText(op1 + " " + operation + " " + op2);
+                    current.setText(Double.toString(prevResult));
+                    operation = "";
+                    break;
+                case "-":
+                    prevResult = op1 - op2;
+                    history.setText(op1 + " " + operation + " " + op2);
+                    current.setText(Double.toString(prevResult));
+                    operation = "";
+                    break;
+                case "*":
+                    prevResult = op1 * op2;
+                    history.setText(op1 + " " + operation + " " + op2);
+                    current.setText(Double.toString(prevResult));
+                    operation = "";
+                    break;
+                case "/":
+                    if(op2 == 0){
+                        current.setError("Input Error");
+                    }
+                    else{
+                        prevResult = op1 / op2;
+                        history.setText(op1 + " " + operation + " " + op2);
+                        current.setText(Double.toString(prevResult));
+                        operation = "";
+                    }
+                    break;
+                case "pow":
+                    prevResult = Math.pow(op1,op2);
+                    history.setText(history.getText().toString() + op2);
+                    current.setText(Double.toString(prevResult));
+                    operation = "";
+                    break;
+            }
+        });
 
+        this.<Button>findViewById(R.id.ansBtn).setOnClickListener(v -> {
+            if(prevResult != 0){ current.setText(Double.toString(prevResult)); }
+        });
+
+        //Cancel buttons
         this.<Button>findViewById(R.id.cancel).setOnClickListener(v -> current.setText("0"));
         this.<Button>findViewById(R.id.cancelAll).setOnClickListener(v -> {
             history.setText("");
             current.setText("0");
             prevResult=0;
         });
+
+        //Text fields that show current operation
         current=findViewById(R.id.insertion);
         history=findViewById(R.id.ans);
     }
